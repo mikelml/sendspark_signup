5544cb89e6fbe4e215008c10458392db5368852c
Mike <miguelshunidgo@hotmail.com>
2024-03-07 14:53:33 -0600

    mdoifiy cors

--- server/index.js
+++ server/index.js
@@ -6,15 +6,12 @@ const PORT = process.env.PORT || 3000;
 const cors = require("cors");
 const router = require("./src/routes.js");
 const http = require('http')
+const cookieParser = require("cookie-parser");
 
-const corsOptions = {
-    origin: '*',
-    credentials: true,
-    optionSuccessStatus: 200,
-}
-
-app.use(cors(corsOptions))
+app.use(cors())
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use('/', router)
 const server = http.createServer(app);
 

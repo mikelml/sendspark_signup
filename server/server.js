85da07cf162a4f970447bbfe6b03fe310f6d7fe1
Mike <miguelshunidgo@hotmail.com>
2024-03-06 23:43:42 -0600

    changin subtitle

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 const express = require('express');
 const { body, validationResult } = require('express-validator');
 const axios = require('axios');
--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import { TextField } from "@mui/material";
 
 const FormInput = ({ formik, name, label, type }) => {
